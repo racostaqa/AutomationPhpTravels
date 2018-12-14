420a02a45b58c538829d7f7eb7464f630b2409f3
racostaqa <racosta@cecropiasolutions.com>
2018-12-14 15:14:29 -0600

    Login with Screenshots when failure occurs.

--- Tests/LoginTest.py
+++ Tests/LoginTest.py
@@ -4,67 +4,64 @@ from Pages.LoginPage import LoginPage
 from Pages.HomePage import HomePage
 from General.Locators import Locators
 from General.EnvironmentSetUp import EnvironmentSetup
+from Utils.ScreenShot import ScreenShot
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from Data.Data import Data
-from time import sleep
 import unittest
 
 
 class LoginTest(EnvironmentSetup):
 
     def test_01_login(self):
-
         # Screen shots
-        # screen_shot_path = Data.LOGIN_SCREEN_SHOTS_PATH
-        # screen_shot = ScreenShot(self.driver)
-        # try:
-        # -------------------
-        # calling Google home page
-        self.driver.get(Data.PHP_TRAVELS_WEB_ADDRESS)
-        self.driver.maximize_window()
-        login_page = LoginPage(self.driver)
+        screen_shot_path = Data.LOGIN_SCREEN_SHOTS_PATH
+        screen_shot = ScreenShot(self.driver)
+        try:
 
-        # waiting for Title element to be present
-        title_element = WebDriverWait(self.driver, 10).until(
-            ec.element_to_be_clickable((By.XPATH, Locators.login_title_x)))
-        login_page.set_title(title_element)
+            print("Navigating to Login Page...")
+            self.driver.get(Data.PHP_TRAVELS_WEB_ADDRESS)
+            self.driver.maximize_window()
 
-        self.assertTrue(login_page.get_title().is_displayed())
-        print("Login page is displayed")
+            # waiting for Title element to be present
+            title_element = WebDriverWait(self.driver, 20).until(
+                ec.element_to_be_clickable((By.XPATH, Locators.login_title_x)))
 
-        # entering credentials
-        login_page.fill_email(Data.LOGIN_ADMIN_EMAIL)
-        login_page.fill_password(Data.LOGIN_VALID_PASSWORD)
+            login_page = LoginPage(self.driver)
+            login_page.set_title(title_element)
 
-    def test_02_login(self):
+            # is_login_page_loaded = login_page.get_title().is_displayed()
+            self.assertTrue(login_page.get_title().is_displayed())
+            print("Login page is displayed")
 
-        # clicking on submit button
+            # entering credentials
+            login_page.fill_email(Data.LOGIN_ADMIN_EMAIL)
+            login_page.fill_password(Data.LOGIN_INVALID_PASSWORD)
+            print("Username and password entered")
 
-        login_page = LoginPage(self.driver)
-        login_page.submit()
-        sleep(3)
-        print("Login in process...")
+            # clicking on submit button
+            login_page.submit()
+            print("Login in process...")
 
-        # verifying Login
-        home_page = HomePage(self.driver)
-        self.assertTrue(home_page.get_title().is_displayed())
-        print("Login successful")
-        # Test completed
-        print("Test completed")
-        # ----------------
-        # except AssertionError as error:
-        #     print("Login failed.\nError: " + str(error))
-        #     screen_shot.take_screen_shot(screen_shot_path+"login.png")
-        #     # Test completed
-        #     print("Test completed")
-        #
-        # except Exception as exception:
-        #     print("Login failed.\nException: " + str(exception))
-        #     screen_shot.take_screen_shot(screen_shot_path+"login.png")
-        #     # Test completed
-        #     print("Test completed")
+            home_page = HomePage(self.driver)
+            # is_home_page_loaded = home_page.get_title().is_displayed()
+            self.assertTrue(home_page.get_title().is_displayed())
+            # self.assertTrue(is_login_page_loaded and is_home_page_loaded,
+            # "Login Page or Home page couldn't be loaded")
+            print("Login successful")
+
+        except AssertionError as error:
+            print("Login failed.\nError: " + str(error))
+            screen_shot.take_screen_shot(screen_shot_path+"login.png")
+            # Test completed
+            print("Test completed")
+
+        except Exception as exception:
+            print("Login failed.\nException: " + str(exception))
+            screen_shot.take_screen_shot(screen_shot_path+"login.png")
+            # Test completed
+            print("Test completed")
 
 
 if __name__ == '__main__':
